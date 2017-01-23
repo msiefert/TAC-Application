0ecd7d8e1fb4764d4990c2a703d59e5defb3c02c
Michael <msiefert@liberty.edu>
2017-01-23 15:32:20 -0500

    removed extra imports

--- app/src/main/java/com/tac/tacapplication/Drawer.java
+++ app/src/main/java/com/tac/tacapplication/Drawer.java
@@ -23,13 +23,6 @@ import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.PopupWindow;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.InetAddress;
-import java.net.Socket;
-
 import static com.tac.tacapplication.R.layout.label;
 import static com.tac.tacapplication.R.layout.nav;
 import static com.tac.tacapplication.R.layout.call;
